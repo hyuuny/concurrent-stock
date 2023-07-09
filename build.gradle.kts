04e416a3d9a576363125ac2007a206ec27022d04
hyuuny <shyune@knou.co.kr>
2023-07-09 13:32:25 +0900

    feat: Redis의 Redisson을 활용하여 동시성 이슈 테스트 기능 구헌

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation ("org.redisson:redisson-spring-boot-starter:3.22.1")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
